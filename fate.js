4cad7197ed8872d0e32b79eb55d96b8b345a4300
RKdev <rkdevmail@gmail.com>
2016-04-26 08:36:09 -0500

    init commit

---
+++ fate.js
@@ -0,0 +1,42 @@
+var players = [];
+
+function Character() {
+    this.player = '';
+    this.name = '';
+    this.class = '';
+    this.race = '';
+    this.strength = 0;
+    this.dex = 0;
+    this.fort = 0;
+    this.const = 0;
+    this.ac = 0;
+    this.hp = 0;
+    this.speed = 0;
+    this.gold = 0;
+    this.items = [];
+    this.weapons = [];
+    this.spells = [];
+    this.casts = 0;
+    this.skills = [];
+    this.feats = [];
+}
+
+Character.prototype.createCharacter = function(){
+    //executes 4d6 dice rolls, throwing out the lowest
+    //prompts user to assign rolls to stats
+    //prompts user for name, class, race,
+    //assigns base stats to character
+};
+Character.prototype.attack = function(weapon, target){
+    //tells fate to iniate an attach against the selected target with the selected weapon
+};
+Character.prototype.cast = function(spell, target){
+    //tells fate to cast the selected spell at the target
+};
+Character.prototype.skill = function(skill){
+    //character evokes a skill
+};
+
+Character.prototype.feat = function(feat){
+    //character performs a feat
+};
